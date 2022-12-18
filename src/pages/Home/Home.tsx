3a4176d440009b7550f66f6b80608a18e7db1f61
Victor Fedorov <vpfedorov25@gmail.com>
2022-12-18 13:00:04 +0400

    Feat: Added big background logo for main show

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import "./home.scss";
 import NavBar from "../../components/ui/Navbar/Navbar";
-import Content from "../../components/pages/content/Content";
+import Content from "../../components/pages/home/content/Content";
 
 const Home = () => {
   return (
     <div className="home">
       <NavBar />
       <Content />
+      <p>Hello world</p>
       {/* <Footer /> */}
     </div>
   );
