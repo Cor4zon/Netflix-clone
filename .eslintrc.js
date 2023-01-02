afd2cafd9408d5666d611b4d231f3459ca0f41df
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-02 18:49:23 +0400

    Fix: temporary freezed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  validate: [ "javascript", "javascriptreact", "html", "typescriptreact" ],
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'config-airbnb'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['react', 'simple-import-sort'],
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error'
-  }
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true
+//   },
+//   extends: ['plugin:react/recommended', 'standard-with-typescript'],
+//   overrides: [],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module'
+//   },
+//   plugins: ['react', 'simple-import-sort'],
+//   rules: {
+//     semi: ['error', 'always'],
+//     quotes: ['error', 'single'],
+//     'simple-import-sort/imports': 'error',
+//     'simple-import-sort/exports': 'error'
+//   }
+// };
