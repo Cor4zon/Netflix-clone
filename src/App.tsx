38128e8f8cbb2f77dda2952dddaf2d7e6c988969
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-02 19:28:16 +0400

    Feat: Start working on #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/SignUp";
 import "./App.scss";
 
 function App() {
   return (
-    <div>
-        <Home />
+    <div className="page-container">
+      {/* <Home /> */}
+      <SignUp />
     </div>
   );
 }
-
-
-
 
 export default App;
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import "./signUp.scss";
+
+const SingUp = () => {
+  return (
+    <div className="wrapper">
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default SingUp;
