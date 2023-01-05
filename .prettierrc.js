a1238d786d8a70445684a161c09444dfb0866837
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-05 19:26:04 +0400

    Feat: Configurate Eslint and Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,12 @@
-// module.exports = {
-//     semi: true,                 // semicolon in the end
-//     trailingComma: 'all',
-//     singleQuote: true,
-//     printWidth: 90,
-//     tabWidth: 2,
-//     jsxBracketSameLine: true,
-//     endOfLine: 'auto',
-//   };
+module.exports = {
+    semi: true,                 // semicolon in the end
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 90,
+    tabWidth: 2,
+    jsxBracketSameLine: true,
+    endOfLine: 'auto',
+    bracketSpacing: true
+  };
+  
   
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import Home from "./pages/Home/Home";
-import SignUp from "./pages/SignUp/SignUp";
-import "./App.scss";
+import Home from './pages/Home/Home';
+
+import SignUp from './pages/SignUp/SignUp';
+import './App.scss';
 
 function App() {
   return (
