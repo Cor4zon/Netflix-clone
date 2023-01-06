4ecbdbe6c2f9d6ceb4723655d5a5e79dbc6ba3f9
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-06 19:18:37 +0400

    Added carousel for cards

--- src/components/ui/CardList/CardList.tsx
+++ src/components/ui/CardList/CardList.tsx
@@ -1,28 +1,39 @@
 import Card from '@/components/ui/Card/Card';
 import './CardList.scss';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 const CardList = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="card-list_wrapper">
-      <h3 className="card-list__title">See again <KeyboardArrowRightIcon /> </h3>
-      <ul className="card_list">
+      <h3 className="card-list__title">
+        See again <KeyboardArrowRightIcon />{' '}
+      </h3>
+      return (
+      <Slider {...settings}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Slider>
+      {/* <ul className="card_list">
         <li className="list_item">
           <Card />
         </li>
-        <li className="list_item">
-          <Card />
-        </li>
-        <li className="list_item">
-          <Card />
-        </li>
-        <li className="list_item">
-          <Card />
-        </li>
-        <li className="list_item">
-          <Card />
-        </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
