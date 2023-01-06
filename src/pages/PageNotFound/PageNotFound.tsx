bf88b9417ace933d7efc97fe4fe62071fe73a339
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-06 18:33:47 +0400

    Added simple page not found page

---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageNotFound.scss';
+
+const PageNotFound = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="text-style">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import SignUp from '@/pages/SignUp/SignUp';
+import PageNotFound from '@/pages/PageNotFound/PageNotFound';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [],
+    errorElement: <PageNotFound />,
   },
   {
     path: '/signup',
