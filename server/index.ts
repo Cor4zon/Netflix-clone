dd723bef67f8fb503fd97cf2d8fd30a0ee2c8a16
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-08 14:23:32 +0400

    Initial mongoose configuration

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,20 @@
-console.log('netflix rest api will be back');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+main().catch(err => console.log(err));
+
+async function main() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('DB connection okey'); 
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+app.listen(8080);
