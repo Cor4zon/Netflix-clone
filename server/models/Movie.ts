98118af2d669ab0bac22b22192c5700eaffc89f8
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-08 14:40:56 +0400

    Added model for Movie, User and List

---
+++ server/models/Movie.ts
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        desc: { type: String },
+        img: {type: String },
+        bigImg: {type: String },
+        titleImg: {type: String },
+        trailer: { type: String },
+        video: { type: String },
+        year: { type: Number, },
+        limit: { type: Number, },
+        genre: { type: String, },
+        isSeries: { type: Boolean, default: false }
+    },
+    { timestamps: true }
+)
+
+export {};
+module.exports = mongoose.model("Movie", MovieSchema);
