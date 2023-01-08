17754e80c9394f6e08f583104696f2fb32c3046b
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-08 19:42:32 +0400

    Finished mvp api for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -29,10 +29,42 @@ router.put("/:id", verify, async (req, res, next) => {
 })
 
 // DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.user.id);
+            res.status(200).json(`User was deleted`);
+        } catch (err) {
+            res.status(500).json("Sever error");
+        }
+    } else {
+        res.status(403).json("You can delete only your own account");
+    }
+})
 
 // GET
+router.get("/find/:id", async (req, res) => {
+    try {
+        const user = User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 // GET ALL
+router.get("/find", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().limit(10) : await User.find();
+            res.status(200).json(users)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+})
+
 
 // GET USER STATS
 
