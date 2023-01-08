7b02b146f4108e976e3cfcac5da618d3fa1434ec
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-08 18:48:45 +0400

    Finished auth and add jwt tokens

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require("crypto-js");
-
+const jwt = require('jsonwebtoken');
 // REGISTER 
 router.post("/register", async (req, res, next) => {
     const cipherPassword = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
@@ -16,7 +16,7 @@ router.post("/register", async (req, res, next) => {
         const user = await newUser.save();
         res.status(201).json(user);
 
-        
+           
     } catch (err) {
         console.log(err);
     }
@@ -36,10 +36,16 @@ router.post('/login', async (req, res) => {
         const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
         const realPassword = bytes.toString(CryptoJS.enc.Utf8);
 
+        const accessToken = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin}, 
+            process.env.SECRET_KEY, 
+            {expiresIn: '5d'}
+        );
+
         const { password, ...info } = user._doc;
 
         if (realPassword === enteredPassword) {
-            res.status(200).json(info);
+            res.status(200).json({ ...info, accessToken });
         } else {
             res.status(401).json("Wrong email or password");
         }
