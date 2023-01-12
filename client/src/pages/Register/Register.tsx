318afb925bc4f9525c77ee39d213c41b5324fe8c
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-12 19:18:36 +0400

    finished registration page header

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,13 +1,43 @@
 import './Register.scss';
 
+import * as React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import LanguageIcon from '@mui/icons-material/Language';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import NetflixLogo from '@/assets/images/netflix-logo.svg';
+
 const Register = () => {
+  const [language, setLanguage] = React.useState('1');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setLanguage(event.target.value);
+  };
+
   return (
-    <div className="wrapper">
       <div className="content">
-        <p>Hello world</p>
+        <div className="registration-header">
+          <img className="registraion__logo-img" src={NetflixLogo} alt="logo" />
+          <div className="registraion-header__btns">
+            <Select
+              value={language}
+              onChange={handleChange}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }}
+              className={"language-select"}
+            >
+              <MenuItem value="1" className="menu-item__lang"> 
+                {/* <LanguageIcon /> */}
+                <em>English</em>
+                <ArrowDropDownIcon />
+              </MenuItem>
+            </Select>
+            <Button className="register-btn" variant="contained">Sign Up</Button>
+          </div>
+        </div>
         
       </div>
-    </div>
   );
 };
 
