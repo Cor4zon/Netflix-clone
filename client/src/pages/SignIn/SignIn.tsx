eddabaa6de37de951efbe89308695fb191291a5a
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-15 22:33:35 +0400

    added mvp version of signin page

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,12 +1,20 @@
 import './SignIn.scss';
+import NeflixLogo from '@/assets/images/netflix-logo.svg';
 
 const SingIn = () => {
   return (
     <div className="signin-wrapper">
       <div className="content">
         <p>Hello world</p>
-
-
+        <img src={NeflixLogo} alt="netflix logo" />
+        <div className='sign-in-container'>
+          <form className="signin-form" action="">
+            <label>Sign In</label>
+            <input type="email" name="email" placeholder='Email and phone number'/>
+            <input type="password" name="password" placeholder='Password' />
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
       </div>
     </div>
   );
