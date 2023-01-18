027db12a22ac62210f6f6fe057e5a02e46c68f5d
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-18 23:40:19 +0400

    working on first request

--- client/src/components/pages/home/content/ShowRecomendations.tsx
+++ client/src/components/pages/home/content/ShowRecomendations.tsx
@@ -1,7 +1,27 @@
 import CardList from '@/components/ui/CardList/CardList';
 import './ShowRecomendations.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const ShowRecomendations = () => {
+const ShowRecomendations = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomList = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? '?type=' + type : ''}${genre ? '&genre=' + genre : ''}`,
+        );
+        console.log(res);
+        setLists(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomList();
+  }, [type, genre]);
+
   return (
     <div className="shows-wrapper">
       <CardList />
