bcb3c171b0d1a10c3713a6d1ec8555ea7bad93f1
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-19 19:51:09 +0400

    wip

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -46,9 +46,10 @@ router.delete("/:id", verify, async (req, res) => {
 })
 
 // GET
-router.get("find/:id", verify, async (req, res) => {
+router.get("/:id", verify, async (req, res) => {
     try {
         const movie = Movie.findById(req.params.id);
+        console.log(movie);
         res.status(200).json(movie);
     } catch (err) {
         res.status(500).json(err);
