79a40e3da7ca0b24508d5adbcfcddb11ae049b2b
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-17 22:28:34 +0400

    Feat: Add simple select

--- client/src/pages/Series/Series.tsx
+++ client/src/pages/Series/Series.tsx
@@ -2,9 +2,17 @@ import CardList from '@/components/ui/CardList/CardList';
 import NavBar from '@/components/ui/Navbar/Navbar';
 import axios from 'axios';
 import React, { useEffect, useState} from 'react';
+import Select from 'react-select';
+
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
 
 const Movies = () => {
-    const [ genre, setGenre ] = useState("");
+    const [ genre, setGenre ] = useState("chocolate");
     const [ lists, setLists ] = useState([]);
     useEffect(() => {
         const getMovies = async () => {
@@ -18,8 +26,19 @@ const Movies = () => {
         }
         getMovies();
     }, [])
+
+    const handleChange = (selectedOption: string) => {
+        setGenre(selectedOption)
+        console.log(`Option selected:`, selectedOption)
+      };
+
     return (
         <div>
+            <Select
+                value={genre}
+                onChange={handleChange}
+                options={options}
+            />
             <NavBar />
             {lists.map((list) => {
                 return <CardList list={list} key={list?.title} />;
