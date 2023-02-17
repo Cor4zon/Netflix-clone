03e316c0ce596bd81a74ad45eb78df129a193dc0
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-17 22:48:00 +0400

    Feat 5: Select series

--- client/src/pages/Series/Series.tsx
+++ client/src/pages/Series/Series.tsx
@@ -5,27 +5,40 @@ import React, { useEffect, useState} from 'react';
 import Select from 'react-select';
 
 
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
+
 
 const Movies = () => {
-    const [ genre, setGenre ] = useState("chocolate");
+    const [ genre, setGenre ] = useState("");
     const [ lists, setLists ] = useState([]);
+    const [ options, setOptions ] = useState([]);
+    // const options = [
+    //     { value: null, label: 'all' },
+    //     { value: 'chocolate', label: 'Chocolate' },
+    //     { value: 'strawberry', label: 'Strawberry' },
+    //     { value: 'vanilla', label: 'Vanilla' },
+    //   ];
+
     useEffect(() => {
         const getMovies = async () => {
-            const results = await axios.get('http://localhost:8080/api/lists?type=Series', {
+            console.log(genre ? `&genre=${genre.value}` : '');
+            console.log(genre);
+            const results = await axios.get(`http://localhost:8080/api/lists?type=Series${genre ? `&genre=${genre.value}` : ''}`, {
                 headers: {
                     token: `JWT ${localStorage.getItem('token')}`,
             }
         });
         console.log(results);
         setLists(results.data);
+        
+        const optionsLists = [];
+        results.data.map(list => {
+            optionsLists.push({ value: list.genre, label: list.title })
+        });
+
+        setOptions(optionsLists);
         }
         getMovies();
-    }, [])
+    }, [genre])
 
     const handleChange = (selectedOption: string) => {
         setGenre(selectedOption)
