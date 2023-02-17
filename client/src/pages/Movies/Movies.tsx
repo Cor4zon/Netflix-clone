b60598f2902029e74d078793346895c42d6e2625
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-17 22:13:33 +0400

    Feat: Add Movies and Series pages content

--- client/src/pages/Movies/Movies.tsx
+++ client/src/pages/Movies/Movies.tsx
@@ -1,13 +1,30 @@
+import CardList from '@/components/ui/CardList/CardList';
 import NavBar from '@/components/ui/Navbar/Navbar';
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Movies = () => {
     const [ genre, setGenre ] = useState("");
+    const [ lists, setLists ] = useState([]);
     
-    // http://localhost:8080/api/lists?type=Movies&genre=Action
+    useEffect(() => {
+        const getMovies = async () => {
+            const results = await axios.get('http://localhost:8080/api/lists?type=Films', {
+                headers: {
+                    token: `JWT ${localStorage.getItem('token')}`,
+            }
+        });
+        console.log(results);
+        setLists(results.data);
+        }
+        getMovies();
+    }, [])
     return (
         <div>
             <NavBar />
+            {lists.map((list) => {
+                return <CardList list={list} key={list?.title} />;
+            })}
         </div>
     );
 };
--- client/src/pages/Series/Series.tsx
+++ client/src/pages/Series/Series.tsx
@@ -1,24 +1,29 @@
+import CardList from '@/components/ui/CardList/CardList';
 import NavBar from '@/components/ui/Navbar/Navbar';
 import axios from 'axios';
 import React, { useEffect, useState} from 'react';
 
 const Movies = () => {
     const [ genre, setGenre ] = useState("");
-
+    const [ lists, setLists ] = useState([]);
     useEffect(() => {
         const getMovies = async () => {
-            const results = await axios.get('http://localhost:8080/api/lists?type=Series&genre=Action', {
+            const results = await axios.get('http://localhost:8080/api/lists?type=Series', {
                 headers: {
                     token: `JWT ${localStorage.getItem('token')}`,
             }
         });
-            setMovie
+        console.log(results);
+        setLists(results.data);
         }
+        getMovies();
     }, [])
-    // 
     return (
         <div>
             <NavBar />
+            {lists.map((list) => {
+                return <CardList list={list} key={list?.title} />;
+            })}
         </div>
     );
 };
