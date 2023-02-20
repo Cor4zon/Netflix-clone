44ebf61a1c36f0a37d25aa91465d4888404cf1da
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-20 22:35:11 +0400

    Added the movie type:

--- client/src/components/ui/Card/Card.tsx
+++ client/src/components/ui/Card/Card.tsx
@@ -3,8 +3,23 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-const Card = ({ movieId }) => {
-  const [movie, setMovie] = useState(null);
+type MovieType = {
+  "_id": number,
+  "title": string,
+  "desc": string,
+  "img": string,
+  "bigImg": string,
+  "titleImg": string,
+  "trailer": string,
+  "video": string,
+  "year": number,
+  "limit": string,
+  "genre": string,
+  "isSeries": boolean
+};
+
+const Card = ({ movieId }: { movieId: number }) => {
+  const [movie, setMovie] = useState<MovieType | null>(null);
 
   useEffect(() => {
     const getMovie = async () => {
@@ -14,20 +29,26 @@ const Card = ({ movieId }) => {
           `JWT ${localStorage.getItem('token')}`,
         },
       });
+      console.log(result.data);
       setMovie(result.data);
     };
     getMovie();
   }, []);
 
   const showCardInfoHanlder = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        
+    // console.log(event.target.id);    
   };
 
   return (
-    <Link to={{ pathname: '/watch'}} state={{movir: movie}}>
-      <div className="card" id={movie?.id} onMouseEnter={(event) => showCardInfoHanlder(event)} style={{backgroundImage: 'url(' + movie?.img + ')'}}>
+    <>
+      <Link to={{ pathname: '/watch'}} state={{movir: movie}}>
+        <div className="card" id={String(movie?._id)} onMouseEnter={(event) => showCardInfoHanlder(event)} style={{backgroundImage: 'url(' + movie?.img + ')'}}>
+        </div>
+      </Link>
+      <div className='bigCard'>
+        <p>h</p>
       </div>
-    </Link>
+    </>
   );
 };
 
